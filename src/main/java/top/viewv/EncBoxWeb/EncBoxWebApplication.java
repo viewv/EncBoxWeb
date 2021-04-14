6854a277d5c41dfea0d56163d6c6c418e61e1abd
viewv <zxnnet@gmail.com>
2021-04-14 16:23:55 +0800

    Try to add upload file function

--- src/main/java/top/viewv/EncBoxWeb/EncBoxWebApplication.java
+++ src/main/java/top/viewv/EncBoxWeb/EncBoxWebApplication.java
@@ -3,10 +3,12 @@ package top.viewv.EncBoxWeb;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAsync
 @RestController
 public class EncBoxWebApplication {
 
