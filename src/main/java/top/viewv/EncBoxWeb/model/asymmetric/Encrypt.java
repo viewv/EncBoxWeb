598ecd0c77141043601d8b760ab92be95f710104
viewv <zxnnet@gmail.com>
2021-04-14 15:24:47 +0800

    Add core class

---
+++ src/main/java/top/viewv/EncBoxWeb/model/asymmetric/Encrypt.java
@@ -0,0 +1,25 @@
+package top.viewv.EncBoxWeb.model.asymmetric;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+
+public class Encrypt {
+    public static byte[] encrypt(byte[] plain, String algorithm, Key publickey) {
+        Security.addProvider(new BouncyCastleProvider());
+        try {
+            Cipher cipher = Cipher.getInstance(algorithm, "BC");
+            cipher.init(Cipher.ENCRYPT_MODE, publickey);
+
+            return cipher.doFinal(plain);
+
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | NoSuchProviderException | BadPaddingException | IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+}
